ef6b124487a67fbf9a168f4113186a3d3c1aaea6
Ivan Krumov <zakz13@gmail.com>
2022-01-16 13:21:51 +0200

    Last changes

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,6 @@ from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 
-class PostList(generics.ListAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
